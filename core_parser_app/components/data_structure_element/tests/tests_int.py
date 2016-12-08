3e17bff01fe2f1d38c6b9d640fe06cbe0f5deed7
Adrien Catel <adrien.catel@nist.gov>
2016-12-08 10:17:19 -0500

    change api call method in test instead model methods
    Change-Id: I5bd08839bfb943a99b236fb817cc7f385835dc14

--- core_parser_app/components/data_structure_element/tests/tests_int.py
+++ core_parser_app/components/data_structure_element/tests/tests_int.py
@@ -1,6 +1,7 @@
 """ Integration test of Data structure element
 """
 from core_parser_app.components.data_structure_element.models import DataStructureElement
+import core_parser_app.components.data_structure_element.api as api_data_structure_element
 from core_parser_app.components.data_structure_element.tests.fixtures.fixtures import DataFixtures
 from core_main_app.utils.integration_tests.integration_base_test_case import MongoIntegrationBaseTestCase
 from core_main_app.commons import exceptions
@@ -15,13 +16,13 @@ class TestDataStructureElementGetAll(MongoIntegrationBaseTestCase):
 
     def test_data_structure_element_get_all_return_collection_of_data(self):
         # Act
-        result = DataStructureElement.get_all()
+        result = api_data_structure_element.get_all()
         # Assert
         self.assertTrue(all(isinstance(item, DataStructureElement) for item in result))
 
     def test_data_structure_element_get_all_return_all_data_structure_element_in_collection(self):
         # Act
-        result = DataStructureElement.get_all()
+        result = api_data_structure_element.get_all()
         # Assert
         self.assertTrue(len(self.fixture.data_structure_element_collection) == len(result))
 
@@ -33,18 +34,18 @@ class TestDataStructureElementGetById(MongoIntegrationBaseTestCase):
     def test_data_get_by_id_raises_does_not_exist_error_if_not_found(self):
         # Act # Assert
         with self.assertRaises(exceptions.DoesNotExist):
-            DataStructureElement.get_by_id(ObjectId())
+            api_data_structure_element.get_by_id(ObjectId())
 
     def test_data_get_by_id_return_data_if_found(self):
         # Act
-        result = DataStructureElement.get_by_id(self.fixture.data_structure_element_collection[1].id)
+        result = api_data_structure_element.get_by_id(self.fixture.data_structure_element_collection[1].id)
         # Assert
         self.assertEqual(result, self.fixture.data_structure_element_collection[1])
 
     def test_data_get_by_id_raises_exception_if_fail(self):
         # Act # Assert
         with self.assertRaises(Exception):
-            DataStructureElement.get_by_id("")
+            api_data_structure_element.get_by_id("")
 
 
 class TestDataStructureElementGetByChildId(MongoIntegrationBaseTestCase):
@@ -53,14 +54,14 @@ class TestDataStructureElementGetByChildId(MongoIntegrationBaseTestCase):
 
     def test_data_structure_element_get_by_child_id(self):
         # Act
-        result = DataStructureElement.get_all_by_child_id(self.fixture.data_structure_element_child_id_1)
+        result = api_data_structure_element.get_all_by_child_id(self.fixture.data_structure_element_child_id_1)
         # Assert
         self.assertTrue(all(isinstance(item, DataStructureElement) for item in result))
 
     def test_data_get_by_child_id_raises_exception_if_fail(self):
         # Act # Assert
         with self.assertRaises(Exception):
-            DataStructureElement.get_all_by_child_id("")
+            api_data_structure_element.get_all_by_child_id("")
 
 
 
