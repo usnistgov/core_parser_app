7ca9afc8d49be424b2d1b60fd692408c84674f8c
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2017-11-13 08:12:22 -0500

    Update version alpha5
    Change-Id: I06a566f74828d65a7d965db2796fc97043357863

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_parser_app",
-    version="1.0.0-alpha4",
+    version="1.0.0-alpha5",
     description="XSD parser tools for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
