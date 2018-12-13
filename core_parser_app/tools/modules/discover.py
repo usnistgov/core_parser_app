76ec5c59d5ef435266a2678c6cbbb6780a86886f
Yande <ndeye.ndiaye@nist.gov>
2018-12-13 15:23:53 -0500

    fix:fix discover function in case RegexURLPattern has no attribute url_patterns
    Change-Id: Ib702c447c0ee5065e026619361f294eb95d75cef

--- core_parser_app/tools/modules/discover.py
+++ core_parser_app/tools/modules/discover.py
@@ -19,22 +19,24 @@ def discover_modules(urls):
     # Look for modules in project urls
     try:
         for url in urls:
-            for url_pattern in url.url_patterns:
-                module_view_name = url_pattern.lookup_str if hasattr(url_pattern, 'lookup_str') else ''
-                if module_view_name.startswith('core_module_'):
-                    module_view = AbstractModule.get_view_from_view_path(module_view_name)
-                    if issubclass(module_view, AbstractModule):
-                        module_object = Module(url=url_pattern.regex.pattern,
-                                               name=url_pattern.name,
-                                               view=module_view_name,
-                                               multiple=module_view.is_managing_occurrences)
-                        module_api.upsert(module_object)
+            if hasattr(url, "url_patterns"):
+                for url_pattern in url.url_patterns:
+                    module_view_name = url_pattern.lookup_str if hasattr(url_pattern, 'lookup_str') else ''
+                    if module_view_name.startswith('core_module_'):
+                        module_view = AbstractModule.get_view_from_view_path(module_view_name)
+                        if issubclass(module_view, AbstractModule):
+                            module_object = Module(url=url_pattern.regex.pattern,
+                                                   name=url_pattern.name,
+                                                   view=module_view_name,
+                                                   multiple=module_view.is_managing_occurrences)
+                            module_api.upsert(module_object)
     except ValidationError:
         # something went wrong, delete already added modules
         module_api.delete_all()
 
         error_msg = 'A validation error occurred during the module discovery. ' \
                     'Please provide a name to all modules urls using the name argument.'
+
         raise ModuleError(error_msg)
     except Exception, e:
         # something went wrong, delete already added modules
