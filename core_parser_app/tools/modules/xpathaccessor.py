04735d566cad0c002e0e1367ce841f3a851c5ae0
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-06-26 11:35:54 -0400

    fix: user defined exceptions to string
    Change-Id: Ia028024a25aaa42faf613b7cf49e4f93a25c8807

--- core_parser_app/tools/modules/xpathaccessor.py
+++ core_parser_app/tools/modules/xpathaccessor.py
@@ -94,6 +94,3 @@ class XPathAccessorError(Exception):
     """
     def __init__(self, message):
         self.message = message
-        
-    def __str__(self):
-        return repr(self.message)
--- core_parser_app/tools/parser/exceptions.py
+++ core_parser_app/tools/parser/exceptions.py
@@ -10,9 +10,6 @@ class ParserError(Exception):
     def __init__(self, message):
         self.message = message
 
-    def __str__(self):
-        return repr(self.message)
-
 
 class RendererError(Exception):
     """
@@ -21,6 +18,3 @@ class RendererError(Exception):
 
     def __init__(self, message):
         self.message = message
-
-    def __str__(self):
-        return repr(self.message)
