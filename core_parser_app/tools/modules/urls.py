4ed7f9beffc641baa584c749c80ec6ddec0bee22
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:46 -0400

    feat: upgrade django 2.2
    Change-Id: I02921432ba903db71b59480b0b951995edaf8bd8

--- core_parser_app/tools/modules/urls.py
+++ core_parser_app/tools/modules/urls.py
@@ -1,12 +1,13 @@
 """Url routing
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_parser_app.tools.modules.views import views as modules_views
 
 urlpatterns = [
-    url(r'^$', modules_views.index,
-        name='core_parser_app_modules'),
-    url(r'^resources', modules_views.load_resources_view,
-        name='core_parser_app_modules_resources'),
+    re_path(r'^$', modules_views.index,
+            name='core_parser_app_modules'),
+    re_path(r'^resources', modules_views.load_resources_view,
+            name='core_parser_app_modules_resources'),
 ]
