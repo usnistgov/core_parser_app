6d915a77fc050d3736f70f224c7afb74d2656cbe
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-06-18 15:41:52 -0400

    fix: add missing admin auth to views
    Change-Id: Id11559f60d25d6bbbc1d783ecd486b5ea42a8155

--- core_parser_app/admin.py
+++ core_parser_app/admin.py
@@ -2,6 +2,7 @@
 Url router for the administration views
 """
 from django.contrib import admin
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import re_path
 
 from core_parser_app.views.admin import views as admin_views
@@ -9,8 +10,10 @@ from core_parser_app.views.admin import views as admin_views
 admin_urls = [
     re_path(
         r"^template/modules/(?P<pk>\w+)",
-        admin_views.ManageModulesAdminView.as_view(
-            back_to_previous_url="admin:core_main_app_manage_template_versions",
+        staff_member_required(
+            admin_views.ManageModulesAdminView.as_view(
+                back_to_previous_url="admin:core_main_app_manage_template_versions",
+            )
         ),
         name="core_parser_app_template_modules",
     ),
