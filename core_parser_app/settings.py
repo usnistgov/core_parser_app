96e2e3856a8fcab36704d6408e435cc6406ffed1
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-07-01 10:13:13 -0400

    fix: limit number of in-memory elements
    Change-Id: Id9337d4c7294fe5a07fed516ea0fcc7ed20b1efe

--- core_parser_app/settings.py
+++ core_parser_app/settings.py
@@ -15,3 +15,10 @@ MODULES_ROOT = join(dirname(realpath(__file__)).replace("\\", "/"), "tools", "mo
 MODULE_TAG_NAME = getattr(settings, "MODULE_TAG_NAME", "module")
 
 AUTO_ESCAPE_XML_ENTITIES = getattr(settings, "AUTO_ESCAPE_XML_ENTITIES", True)
+
+PARSER_MAX_IN_MEMORY_ELEMENTS = getattr(
+    settings, "PARSER_MAX_IN_MEMORY_ELEMENTS", 10000
+)
+""" Maximum number of in-memory elements allowed by the system during the parsing of XML/XSD files.
+A large number of elements may cause performance issues.
+"""
